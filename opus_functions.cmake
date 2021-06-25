d33891ee806df7b0ec39250aa77cca6857f0a211
Julien Wadel <julien.wadel@belledonne-communications.com>
2021-06-25 16:44:32 +0200

    Apple ARM64 build and presuming Neon on it

--- opus_functions.cmake
+++ opus_functions.cmake
@@ -173,7 +173,7 @@ function(opus_detect_sse COMPILER_SUPPORT_SIMD)
 endfunction()
 
 function(opus_detect_neon COMPILER_SUPPORT_NEON)
-  if(CMAKE_SYSTEM_PROCESSOR MATCHES "(armv7|armv7-a|armv7s|aarch64)")
+  if(CMAKE_SYSTEM_PROCESSOR MATCHES "(armv7|armv7-a|armv7s|aarch64|arm64)")
     message(STATUS "Check NEON support by compiler")
     check_include_file(arm_neon.h HAVE_ARM_NEON_H)
     if(HAVE_ARM_NEON_H)
